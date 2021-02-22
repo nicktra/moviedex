2e967c32a1cd9adce4947d687cad77d4ac0bdcec
baloh <balohbaloh@yahoo.co.id>
2021-02-22 10:56:16 +0700

    Add Encryption SQLCipher CoreModule

--- core/src/main/java/com/nicktra/moviedex/core/di/CoreModule.kt
+++ core/src/main/java/com/nicktra/moviedex/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.nicktra.moviedex.core.data.source.remote.RemoteDataSource
 import com.nicktra.moviedex.core.data.source.remote.network.ApiService
 import com.nicktra.moviedex.core.domain.repository.IMovieRepository
 import com.nicktra.moviedex.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,10 +21,14 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("nicktra".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieDatabase::class.java, "Moviedex.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
